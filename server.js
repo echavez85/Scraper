79f569a418acaa2ff112bc74335d0c840277685c
echavez85 <echavez85@gmail.com>
2019-03-02 18:06:02 -0600

    connected mongoose and required stuff.

--- server.js
+++ server.js
@@ -15,6 +15,21 @@ const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/shroud
 const app = express();
 
 // configure handlebars
-const handlebars = require('express-handlebars');
+const expressHandlebars = require('express-handlebars');
 
+app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// use morgan to log requests
+app.use(logger('dev'));
+// use express static to keep public folder as statid directory
+app.use(express.static('public'));
+
+// connect to mongo db w/ promise
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+    useMongoClient: true
+});
+
+// set routes
 
